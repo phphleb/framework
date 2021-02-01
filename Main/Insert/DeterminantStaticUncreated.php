3b379ae9ed6bc2cb08ca38bf85d9342d1fb7b9aa
Foma Tuturov <fomiash@yandex.ru>
2021-02-01 13:43:01 +0300

    new version

--- Main/Insert/DeterminantStaticUncreated.php
+++ Main/Insert/DeterminantStaticUncreated.php
@@ -3,25 +3,24 @@
 declare(strict_types=1);
 
 /*
- * Forming singleton trait.
+ * Forming pseudo-singleton trait.
  *
- * Трейт формирующий синглетон.
+ * Трейт формирующий псевдо-синглетон.
  */
 
 trait DeterminantStaticUncreated
 {
     private static $instance;
 
-    protected function __construct() { }
+    protected function __construct() {}
 
-    protected function __clone() { }
+    protected function __clone() {}
 
     public static function instance() {
         if (is_null(self::$instance)) {
             self::$instance = new static();
-        }
-        if(self::$instance instanceof Closure) {
-            return self::$instance->call(new static);
+        } elseif (is_bool(self::$instance)) {
+            throw new \Exception('Object is destruct');
         }
         return self::$instance;
     }
