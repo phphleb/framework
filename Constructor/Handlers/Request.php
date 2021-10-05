726d1f4e97a7429cdc19f3a16c7d22f33730ec0c
Foma Tuturov <fomiash@yandex.ru>
2021-10-05 15:24:56 +0300

    upd request

--- Constructor/Handlers/Request.php
+++ Constructor/Handlers/Request.php
@@ -295,7 +295,7 @@ final class Request extends BaseSingleton
      * @return bool
      */
     public static function isXmlHttpRequest() {
-        return $_SERVER['HTTP-X-REQUESTED-WITH'] == 'XMLHttpRequest';
+        return strtolower($_SERVER['HTTP_X_REQUESTED_WITH'] ?? '') === 'xmlhttprequest';
     }
 
     /**
