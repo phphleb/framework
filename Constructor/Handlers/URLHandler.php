673614b942a9d068d728170f4bf4fa3e16f4122f
Foma Tuturov <fomiash@yandex.ru>
2022-11-25 10:41:04 +0300

    fix bottleneck

--- Constructor/Handlers/URLHandler.php
+++ Constructor/Handlers/URLHandler.php
@@ -184,10 +184,11 @@ final class URLHandler
 
         if ($searchBottleneck) {
             if (isset($block['add']['redirect'])) {
-                if ($this->trimEndSlash($dataPath) === $resultUrl) {
+                $redirectUrl = $this->compoundUrl([$url, $dataPath]);
+                if ($this->trimEndSlash($redirectUrl) === $resultUrl) {
                     return $block;
                 }
-                hleb_redirect(Url::getStandardUrl($dataPath), $block['add']['redirect']);
+                hleb_redirect(Url::getStandardUrl($redirectUrl), $block['add']['redirect']);
             }
             return false;
         }
