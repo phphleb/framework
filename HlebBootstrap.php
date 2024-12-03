e77a6a346d93784c8989c49bf5727c6c7eb93211
Foma Tuturov <fomiash@yandex.ru>
2024-12-04 01:20:44 +0300

    upd bootstrap

--- HlebBootstrap.php
+++ HlebBootstrap.php
@@ -104,7 +104,7 @@ class HlebBootstrap
 
         // The current version of the framework.
         // Текущая версия фреймворка.
-        \defined('HLEB_CORE_VERSION') or \define('HLEB_CORE_VERSION', '2.0.48');
+        \defined('HLEB_CORE_VERSION') or \define('HLEB_CORE_VERSION', '2.0.47');
 
         $this->logger = $logger;
 
