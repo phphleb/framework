40cfafc1cda50bf50ae24f4c5dd73011e5e054b3
Foma Tuturov <fomiash@yandex.ru>
2025-09-13 02:50:42 +0300

    FIX: add route class

---
+++ Main/Routes/ExampleRouteClass.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hleb\Main\Routes;
+
+class ExampleRouteClass
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public static function getData(): array
+    {
+        return [];
+    }
+}
